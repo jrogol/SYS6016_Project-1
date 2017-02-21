7608b02fb82dfbead8a04667f8230bbbf6e1abf2
jrogol <rogol@virginia.edu>
2017-02-21 11:34:05 -0500

    Initial Commit
    Ingest Data from WaPo GitHub, with preliminary data cleaning

---
+++ Ingest.R
@@ -0,0 +1,21 @@
+library(readr)
+library(dplyr)
+library(lubridate)
+
+# From: https://github.com/washingtonpost/data-police-shootings
+shootings <- read_csv('https://github.com/washingtonpost/data-police-shootings/raw/master/fatal-police-shootings-data.csv')
+
+
+# Columns to factor
+factors <- c(4,5,7:14)
+
+shootings[, factors] <- lapply(shootings[, factors], as.factor)
+
+# Aggregate armed into broader categories?
+
+# Extract Weekday, Month and Year from date.
+shootings <- shootings %>%
+        mutate(wday = wday(date, label = T), 
+               month = month(date, label =T),
+               year = year(date))
+
