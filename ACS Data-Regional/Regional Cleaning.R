fde72a6dd9826751722e88861d6b96b9f2df8bca
jrogol <rogol@virginia.edu>
2017-02-26 20:13:32 -0500

    Prepping for rules by State

--- ACS Data-Regional/Regional Cleaning.R
+++ ACS Data-Regional/Regional Cleaning.R
@@ -1,6 +1,7 @@
 library(readr)
 library(dplyr)
 library(lubridate)
+library(arules)
 
 
 #### Read in the Initial Data ####
@@ -28,7 +29,7 @@ shootings <- shootings %>%
 
 # ACS data obtained from:
 # https://factfinder.census.gov/faces/nav/jsf/pages/searchresults.xhtml?refresh=t
-# Data was broken down regionally.
+# Data was broken down by State
 
 # Create vectors containing the states within each region, add a new variable to
 # 'shootings' containing the appropriate region
@@ -54,9 +55,9 @@ shootings$region <-
 
 # Read in the 'Housing' and 'Housing and Demographic' ACS data
 
-ACS_Housing <- read_csv('ACS Data-Regional/ACS_15_1YR_DP04_with_ann.csv',
+ACS_Housing <- read_csv('ACS Data-Regional/ACS_byState/ACS_15_1YR_DP04_with_ann.csv',
                         na = c('','NA','(X)','N'),skip = 1)
-ACS_Demo <- read_csv('ACS Data-Regional/ACS_15_1YR_DP05_with_ann.csv',
+ACS_Demo <- read_csv('ACS Data-Regional/ACS_byState/ACS_15_1YR_DP05_with_ann.csv',
                      na = c('','NA','(X)','N'),skip = 1)
 
 # ACS comes with a metadata CSV, listing all the included variables by name. 
@@ -80,12 +81,77 @@ Demo_keeps <- keeps('ACS Data-Regional/ACS_15_1YR_DP05_metadata.csv')
 ACS_combined <- inner_join(ACS_Housing[,Housing_keeps],
                            ACS_Demo[,Demo_keeps],
                            by = 'Geography') %>%
-                rename(region = Geography)
+                rename(state_name = Geography)
 
-# Join the ACS data with the shooting data (on region)
+# To Join The ACS data with the shootings data, we must pull in state names and
+# abbreviations
 
-shootings_joined <- inner_join(shootings, ACS_combined,
-                               by = 'region')
+# Function to create a list of State abbreviations and names to standardize
+# between ACS and shootings data
+statenames <- function(){
+  data(state)
+  state_names <- cbind(state.abb, state.name)
+  state_names <- as.data.frame(rbind(state_names, c("DC","District of Columbia")))
+  names(state_names) <- c("abb","state_name")
+  state_names
+}
+
+states <- statenames()
+
+# Join shootings with the state names
+shootings <- inner_join(shootings, states,
+                               by = c('state' = 'abb'))
+
+# Filter for 2015 and 2016 only
+shootings_filter <- shootings %>% filter(year <= 2016)
+
+
+# Look at the number of deaths, aggregated by state
+state_count <- shootings_filter %>%
+  group_by(state) %>% 
+  count() %>% 
+  arrange(desc(n))
+
+# Look for states whose cumulative total is more than 2 standard deviations
+# above the state mean
+state_count %>% 
+  filter(n >= mean(n)+2*sd(n))
+
+# Create a new variable separating CA and TX from other states!
+two_sd <- (state_count %>% 
+  filter(n >= mean(n)+2*sd(n)))$state
+
+shootings_filter <- shootings_filter %>% 
+  mutate(outliers = state %in% two_sd)
+
+
+#### Prep for arules ####
+
+# Turn everything into factors
+shootings_filter[,] <- lapply(shootings_filter[,], as.factor)
+
+# Create the transactions
+shooting_trans <- as(shootings_filter %>% select(
+  -id, -name, -date, -state_name, -age, -city, -state, -region), 'transactions')
+
+
+# Create Some rules!
+test <- apriori(shooting_trans, parameter = list(supp = .01, conf = .25,
+                                         minlen =2,maxlen=90, target = 'rules'))
+
+
+test_sub <- subset(test, subset = rhs %pin% 'outliers' & lhs %pin% 'age')
+
+inspect(head(sort(test_sub, by = 'confidence')))
+inspect(test_sub)
+
+
+
+
+
+
+
+
 
 #### Use Decision Tree to Predict XXXX ####
 
@@ -96,13 +162,3 @@ shooting_tree <- C5.0.formula(c(race, gender)~, shootings_joined,
 
 shooting_tree
 summary(shooting_tree)
-
-## Step 4: Evaluating model performance ----
-# create a factor vector of predictions on test data
-credit_pred <- predict(credit_model, credit_test)
-
-# cross tabulation of predicted versus actual classes
-library(gmodels)
-CrossTable(credit_test$default, credit_pred,
-           prop.chisq = FALSE, prop.c = FALSE, prop.r = FALSE,
-           dnn = c('actual default', 'predicted default'))
