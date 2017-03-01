6c1af7ac8d036e00437be5aff2a011970acc1dbc
kaleyhanrahan <kaley.hanrahan@gmail.com>
2017-03-01 17:17:09 -0500

    Updated classification code with cross validation

--- Prediction_Code/Project1_Classification.R
+++ Prediction_Code/Project1_Classification.R
@@ -1,3 +1,6 @@
+##### Machine Learning Project 1 - Classification
+##### Kaley Hanrahan (knh9yd), James Rogol (jr2ef), Aksheetha Srihdar (as8zb), Megan Stiles (mes5ac)
+
 library(readr)
 library(dplyr)
 library(lubridate)
@@ -11,6 +14,16 @@ library(e1071) # Naive Bayes
 
 setwd('/Users/meganstiles/Desktop/Data_Science/Spring_2017/Machine Learning/Project_1/')
 setwd("~/UVaMSDS/MachineLearning/Project1")
+
+### Preliminary Cleaning
+shootings <- read_csv('https://github.com/washingtonpost/data-police-shootings/raw/master/fatal-police-shootings-data.csv')
+factors <- c(4,5,7:14)
+shootings[, factors] <- lapply(shootings[, factors], as.factor)
+shootings <- shootings[, -c(1:4,9,10,14)]
+
+# Because the data was updated regularly, we cleaned the data and saved a csv file
+# on February 23 so that our data and analysis would stay constant.
+write_csv(shootings, 'Shootings_Data.csv')
 
 #Read in clean csv
 shootings<- read.csv('Shootings_Data.csv')
@@ -39,18 +52,12 @@ naCheck$race <- abs(is.na(naCheck$race))
 naCheck$age <- abs(is.na(naCheck$age))
 naCheck$flee <- abs(is.na(naCheck$flee))
 #all numeric for correlation matrix
-#naCheck$manner_of_death <- as.numeric(naCheck$manner_of_death)
 naCheck$gender <- as.numeric(naCheck$gender)
 naCheck$signs_of_mental_illness <- as.numeric(naCheck$signs_of_mental_illness)
 naCheck$threat_level <- as.numeric(naCheck$threat_level)
-#naCheck$body_camera <- as.numeric(naCheck$body_camera)
 cor(naCheck)
 str(naCheck)
 # there does not appear to be any patterns with missing values - evidence for missing at random
-################
-### We removed manner of death and body camera?
-################
-
 
 ##Drop missing values from Race
 shootings<- shootings[!is.na(shootings$race),]
@@ -61,14 +68,11 @@ shootings<- shootings[!is.na(shootings$flee),]
 #Perform Mean Imputation on Age Variable
 shootings$age[is.na(shootings$age)]<- mean(shootings$age, na.rm=TRUE)
 sum(is.na(shootings$age))
-################ NO LONGER RELEVANT
-#Remove Missing Armed Values
-#shootings<- shootings[!is.na(shootings$armed),]
-################
 
 table(shootings$signs_of_mental_illness)/nrow(shootings)
 
 ####Set Training and Testing Sets
+set.seed(7)
 training.indices = sample(1:nrow(shootings), as.integer(nrow(shootings) * 0.80))
 training.set = shootings[training.indices,]
 testing.set = shootings[-training.indices,]
@@ -83,14 +87,30 @@ rf.fit <- randomForest(signs_of_mental_illness ~., data = training.set)
 predictions = predict(rf.fit, newdata = testing.set)
 
 # Output raw accuracy.
-sum(predictions == testing.set[,"signs_of_mental_illness"]) / nrow(testing.set) #72.43%
+sum(predictions == testing.set[,"signs_of_mental_illness"]) / nrow(testing.set) #72.5%
+
+# Confusion Matrix
+table(predictions, testing.set$signs_of_mental_illness)
+##  predictions False True
+##  False       288  109
+##  True        1    2
+
+# Sensitivity
+table(predictions,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+# 1.8%
+
+#This accuracy is due to the model just predicting False for "signs of mental illness" each time.
+#The data is approximately 75% False for mental illness. To account for this we will look at
+# oversampling the number of True signs of mental illness for our training set
+
 
 #10-Fold CV
 
 #Create Folds
-folds<- createFolds(shootings$attackGun, k=10, list = TRUE, returnTrain = FALSE)
+folds<- createFolds(shootings$signs_of_mental_illness, k=10, list = TRUE, returnTrain = FALSE)
 
 raw_accuracy<- vector()
+raw_sensitivity<- vector()
 i=0
 for (i in 1:10) {
   #Create testing indicies based on folds
@@ -112,25 +132,37 @@ for (i in 1:10) {
   
   #Store accuracy for each run in vector
   raw_accuracy[i]= accuracy
+  
+  #Calculate sensitivity
+  sensitivity <- table(predictions,test$signs_of_mental_illness)[2,2]/table(test$signs_of_mental_illness)[2]
+  #Store sensitivity for each run in vector
+  raw_sensitivity[i]= sensitivity
 }
 raw_accuracy
-mean(raw_accuracy) #75.911
-
-#This accuracy is due to the model just predicting False for "signs of mental illness" each time.
-#The data is approximately 75% False for mental illness. To account for this we will look at
-# oversampling the number of True signs of mental illness for our training set
+mean(raw_accuracy) # 76.3%
+raw_sensitivity
+mean(raw_sensitivity) # 5.6%
 
 #####################
 #Logistic Regression#
 #####################
 lm1 <- glm(signs_of_mental_illness ~ . , family=binomial, data=training.set)
 yhat <- predict(lm1, newdata = testing.set, type="response")
-glm.pred <- as.factor(ifelse(yhat>0.4, 'True', 'False'))
+glm.pred <- as.factor(ifelse(yhat>0.5, 'True', 'False'))
 
 num.correct = sum(glm.pred == testing.set$signs_of_mental_illness)
-accuracy = num.correct / nrow(testing.set) 
-### Accuracy = 0.717
+num.correct / nrow(testing.set) 
+### Accuracy = 72.75%
 table(glm.pred, testing.set$signs_of_mental_illness)
+
+# Confusion Matrix
+## glm.pred   False True
+## False      286   106
+## True       3     5
+
+# Sensitivity
+table(glm.pred,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+# = 4.5%
 
 #Create ROCR prediction object and extract ROC curve and AUC
 pred <- prediction(yhat, testing.set$signs_of_mental_illness)    
@@ -139,7 +171,45 @@ auc = performance(pred, "auc")@y.values[[1]]
 plot(perf, col=rainbow(7), main = paste("ROC Curve: Logistic (AUC=", round(auc,2), ")", sep=""), xlab="FPR", 
      ylab="TPR")    
 abline(0, 1, lty="dashed") #add a 45 degree line
-### AUC = 0.66
+### AUC = 0.73
+
+#10-Fold CV
+
+#Create Folds
+folds<- createFolds(shootings$signs_of_mental_illness, k=10, list = TRUE, returnTrain = FALSE)
+
+set.seed(8)
+raw_accuracy<- vector()
+raw_sensitivity<- vector()
+i=0
+for (i in 1:10) {
+  #Create testing indicies based on folds
+  test.indices<- folds[[i]]
+  
+  #Create training and testing sets
+  train = shootings[-test.indices,]
+  test = shootings[test.indices,]
+  
+  # Fit logistic regression
+  lm1 <- glm(signs_of_mental_illness ~ . , family=binomial, data=train)
+  # Predict testing data.
+  yhat <- predict(lm1, newdata = test, type="response")
+  glm.pred <- as.factor(ifelse(yhat>0.5, 'True', 'False'))
+  
+  #Calculate Accuracy
+  accuracy<-sum(glm.pred == test[,"signs_of_mental_illness"]) / nrow(test)
+  #Store accuracy for each run in vector
+  raw_accuracy[i]= accuracy
+  
+  #Calculate sensitivity
+  sensitivity <- table(glm.pred,test$signs_of_mental_illness)[2,2]/table(test$signs_of_mental_illness)[2]
+  #Store sensitivity for each run in vector
+  raw_sensitivity[i]= sensitivity
+}
+raw_accuracy
+mean(raw_accuracy) # 75.3%
+raw_sensitivity
+mean(raw_sensitivity) # 6.4%
 
 #############
 #Naive Bayes#
@@ -148,21 +218,60 @@ model <- naiveBayes(signs_of_mental_illness ~ ., training.set)
 yhat <- predict(model, testing.set, type = "class")
 
 num.correct = sum(yhat == testing.set$signs_of_mental_illness)
-accuracy = num.correct / nrow(testing.set)
-### Accuracy = 0.736
+num.correct / nrow(testing.set)
+### Accuracy = 73.5%
 table(yhat, testing.set$signs_of_mental_illness)
 
-#############
-#    SVM    #
-#############
-# Fit SVM.
-svm.fit <- ksvm(signs_of_mental_illness ~ ., data=training.set, type="C-svc", kernel="rbfdot", C=50)
-# Predict testing data.
-predictions = predict(svm.fit, newdata=testing.set)
-# Output raw accuracy.
-sum(predictions == testing.set$signs_of_mental_illness) / nrow(testing.set)
-### Accuracy = 0.742
-table(predictions, testing.set$signs_of_mental_illness)
+# Confusion Matrix
+##yhat    False  True
+##False   274    91
+##True    15     20
+
+# Sensitivity
+table(yhat,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+# Sensitivity = 18%
+
+#10-Fold CV
+
+#Create Folds
+folds<- createFolds(shootings$signs_of_mental_illness, k=10, list = TRUE, returnTrain = FALSE)
+
+raw_accuracy<- vector()
+raw_sensitivity<- vector()
+i=0
+for (i in 1:10) {
+  #Create testing indicies based on folds
+  test.indices<- folds[[i]]
+  
+  #Create training and testing sets
+  train = shootings[-test.indices,]
+  test = shootings[test.indices,]
+  
+  #train Model
+  model <- naiveBayes(signs_of_mental_illness ~ ., train)
+  yhat <- predict(model, testing.set, type = "class")
+  
+  #Make predictions based on model for testing set
+  predictions<- predict(rf.fit, newdata = test)
+  
+  #Calculate Accuracy
+  
+  accuracy<-sum(predictions == test[,"signs_of_mental_illness"]) / nrow(test)
+  
+  #Store accuracy for each run in vector
+  raw_accuracy[i]= accuracy
+  
+  #Calculate sensitivity
+  sensitivity <- table(predictions,test$signs_of_mental_illness)[2,2]/table(test$signs_of_mental_illness)[2]
+  #Store sensitivity for each run in vector
+  raw_sensitivity[i]= sensitivity
+}
+raw_accuracy
+mean(raw_accuracy) # 76.3%
+raw_sensitivity
+mean(raw_sensitivity) # 5.6%
+
+
 
 
 #########################################
@@ -170,15 +279,22 @@ table(predictions, testing.set$signs_of_mental_illness)
 #########################################
 
 mental<- shootings
+table(mental$signs_of_mental_illness)
 
-true.index<- sample(which(mental$signs_of_mental_illness == 'True'), 450)
-false.index<- sample(which(mental$signs_of_mental_illness == 'False'),1000)
+set.seed(7)
+true.index <- which(mental$signs_of_mental_illness=='True')
+false.index <- sample(which(mental$signs_of_mental_illness == 'False'), 700)
 total.index<- c(true.index, false.index)
+undersampled <- mental[total.index,]
+  
+training.indices = sample(1:nrow(undersampled), as.integer(nrow(undersampled) * 0.80))
+training.set = undersampled[training.indices,]
+testing.set = undersampled[-training.indices,]
 
-training.set = mental[total.index,]
-testing.set = mental[-total.index,]
-
+## Distribution of new sample
 table(training.set$signs_of_mental_illness)/nrow(training.set)
+#False      True 
+#0.5797706  0.4202294 
 table(testing.set$signs_of_mental_illness)/nrow(testing.set)
 
 # Fit random forest
@@ -188,22 +304,32 @@ rf.fit <- randomForest(signs_of_mental_illness ~., data = training.set)
 predictions = predict(rf.fit, newdata = testing.set)
 
 # Output raw accuracy.
-sum(predictions == testing.set[,"signs_of_mental_illness"]) / nrow(testing.set) #86.14%
+sum(predictions == testing.set[,"signs_of_mental_illness"]) / nrow(testing.set) #65.42%
 
 table(predictions,testing.set$signs_of_mental_illness)
+## predictions False  True
+## False       94     43
+## True        40     63
+
+# Sensitivity
+table(predictions,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+# = 59.4%
 
 #Validation
 set.seed(8)
 raw_accuracy<- vector()
+raw_sensitivity<- vector()
 i=0
 for (i in 1:10) {
   #Create testing indicies 
-  true.index<- sample(which(mental$signs_of_mental_illness == 'True'), 450)
-  false.index<- sample(which(mental$signs_of_mental_illness == 'False'),1000)
+  true.index <- which(mental$signs_of_mental_illness=='True')
+  false.index <- sample(which(mental$signs_of_mental_illness == 'False'), 700)
   total.index<- c(true.index, false.index)
+  undersampled <- mental[total.index,]
   
-  training.set = mental[total.index,]
-  testing.set = mental[-total.index,]
+  training.indices = sample(1:nrow(undersampled), as.integer(nrow(undersampled) * 0.80))
+  training.set = undersampled[training.indices,]
+  testing.set = undersampled[-training.indices,]
   
   # Fit random forest
   rf.fit <- randomForest(signs_of_mental_illness ~., data = training.set)
@@ -213,23 +339,33 @@ for (i in 1:10) {
   
   #Calculate Accuracy
   accuracy<-sum(predictions == testing.set[,"signs_of_mental_illness"]) / nrow(testing.set)
-  
   #Store accuracy for each run in vector
   raw_accuracy[i]= accuracy
+  
+  #Calculate sensitivity
+  sensitivity <- table(predictions,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+  #Store sensitivity for each run in vector
+  raw_sensitivity[i]= sensitivity
 }
 raw_accuracy
-mean(raw_accuracy) #85.56%
+mean(raw_accuracy) # 63.7%
+raw_sensitivity
+mean(raw_sensitivity) # 58.4%
 
 #####################
 #Logistic Regression#
 #####################
 lm1 <- glm(signs_of_mental_illness ~ . , family=binomial, data=training.set)
 yhat <- predict(lm1, newdata = testing.set, type="response")
-glm.pred <- as.factor(ifelse(yhat>0.4, 'True', 'False'))
+glm.pred <- as.factor(ifelse(yhat>0.5, 'True', 'False'))
 num.correct = sum(glm.pred == testing.set$signs_of_mental_illness)
-accuracy = num.correct / nrow(testing.set)
-### Accuracy = 0.771
+num.correct / nrow(testing.set)
+### Accuracy = 65%
 table(glm.pred, testing.set$signs_of_mental_illness)
+
+# Sensitivity
+table(glm.pred,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+# = 58.7%
 
 # create ROCR prediction object and extract ROC curve and AUC
 pred <- prediction(yhat, testing.set$signs_of_mental_illness)    
@@ -238,7 +374,45 @@ auc = performance(pred, "auc")@y.values[[1]]
 plot(perf, col=rainbow(7), main = paste("ROC Curve: Logistic (AUC=", round(auc,2), ")", sep=""), xlab="FPR", 
      ylab="TPR")    
 abline(0, 1, lty="dashed") #add a 45 degree line
-### AUC = 0.76
+### AUC = 0.71
+
+#Validation
+set.seed(8)
+raw_accuracy<- vector()
+raw_sensitivity<- vector()
+i=0
+for (i in 1:10) {
+  #Create testing indicies 
+  true.index <- which(mental$signs_of_mental_illness=='True')
+  false.index <- sample(which(mental$signs_of_mental_illness == 'False'), 700)
+  total.index<- c(true.index, false.index)
+  undersampled <- mental[total.index,]
+  
+  training.indices = sample(1:nrow(undersampled), as.integer(nrow(undersampled) * 0.80))
+  training.set = undersampled[training.indices,]
+  testing.set = undersampled[-training.indices,]
+  
+  # Fit model
+  lm1 <- glm(signs_of_mental_illness ~ . , family=binomial, data=training.set)
+  # Predict testing data.
+  yhat <- predict(lm1, newdata = testing.set, type="response")
+  glm.pred <- as.factor(ifelse(yhat>0.5, 'True', 'False'))
+  
+  #Calculate Accuracy
+  accuracy<-sum(glm.pred == testing.set[,"signs_of_mental_illness"]) / nrow(testing.set)
+  #Store accuracy for each run in vector
+  raw_accuracy[i]= accuracy
+  
+  #Calculate sensitivity
+  sensitivity <- table(glm.pred,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+  #Store sensitivity for each run in vector
+  raw_sensitivity[i]= sensitivity
+}
+raw_accuracy
+mean(raw_accuracy) # 63.6%
+raw_sensitivity
+mean(raw_sensitivity) # 56.9%
+
 
 #############
 #Naive Bayes#
@@ -248,17 +422,49 @@ yhat <- predict(model, testing.set, type = "class")
 
 table(yhat, testing.set$signs_of_mental_illness)
 num.correct = sum(yhat == testing.set$signs_of_mental_illness)
-accuracy = num.correct / nrow(testing.set)
-### Accuracy = 0.815
+num.correct / nrow(testing.set)
+### Accuracy = 66.25%
 
-#############
-#    SVM    #
-#############
-# Fit SVM.
-svm.fit <- ksvm(signs_of_mental_illness ~ ., data=training.set, type="C-svc", kernel="rbfdot", C=50)
-# Predict testing data.
-predictions = predict(svm.fit, newdata=testing.set)
-# Output raw accuracy.
-sum(predictions == testing.set$signs_of_mental_illness) / nrow(testing.set)
-table(predictions, testing.set$signs_of_mental_illness)
-### Accuracy = 0.774
+# Confusion Matrix
+## yhat    False  True
+## False    92    42
+## True     39    67
+
+# Sensitivity
+table(yhat,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+# =61.5%
+
+#Validation
+set.seed(8)
+raw_accuracy<- vector()
+raw_sensitivity<- vector()
+i=0
+for (i in 1:10) {
+  #Create testing indicies 
+  true.index <- which(mental$signs_of_mental_illness=='True')
+  false.index <- sample(which(mental$signs_of_mental_illness == 'False'), 700)
+  total.index<- c(true.index, false.index)
+  undersampled <- mental[total.index,]
+  
+  training.indices = sample(1:nrow(undersampled), as.integer(nrow(undersampled) * 0.80))
+  training.set = undersampled[training.indices,]
+  testing.set = undersampled[-training.indices,]
+  
+  # Fit model
+  model <- naiveBayes(signs_of_mental_illness ~ ., training.set)
+  yhat <- predict(model, testing.set, type = "class")
+  
+  #Calculate Accuracy
+  accuracy<-sum(yhat == testing.set[,"signs_of_mental_illness"]) / nrow(testing.set)
+  #Store accuracy for each run in vector
+  raw_accuracy[i]= accuracy
+  
+  #Calculate sensitivity
+  sensitivity <- table(yhat,testing.set$signs_of_mental_illness)[2,2]/table(testing.set$signs_of_mental_illness)[2]
+  #Store sensitivity for each run in vector
+  raw_sensitivity[i]= sensitivity
+}
+raw_accuracy
+mean(raw_accuracy) # 64.0%
+raw_sensitivity
+mean(raw_sensitivity) # 54.5%
